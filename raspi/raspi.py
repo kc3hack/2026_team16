f2acd8f07e2a2c15325593bcb172ce66eae480f4
yoyukoka <167397586+yoyukoka@users.noreply.github.com>
2026-02-22 12:02:33 +0900

    時間ランダムに変更

--- raspi/raspi.py
+++ raspi/raspi.py
@@ -117,7 +117,8 @@ async def evaluate_clicks():
 
     if count == 1:
         print("🟢 [ボタン] 1回短押し → 攻撃実行")
-        await executor.execute(offset=0)
+        offset = random.randint(1, 4) * 30
+        await executor.execute(offset)
     elif count == 2:
         print("🔴 [ボタン] 2回短押し → 復旧実行")
         await execute_restore_action()
